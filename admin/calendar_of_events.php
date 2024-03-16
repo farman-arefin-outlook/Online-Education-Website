da43022eea336774583b8aeb8079eef441e32c87
Tamim <farman.arefin1@outlook.com>
2024-03-16 19:23:32 +0600

    Changes calendarOfEvents

--- admin/calendar_of_events.php
+++ admin/calendar_of_events.php
@@ -6,13 +6,13 @@
             <div class="row-fluid">
 					<?php include('sidebar_calendar.php'); ?>
                 
-                <!--/span-->
+                
                 <div class="span9" id="content">
 								        <div id="block_bg" class="block">
                 
 								<div class="block-content collapse in">
 										<div class="span8">
-							<!-- block -->
+							
 										<div class="navbar navbar-inner block-header">
 											<div class="muted pull-left">Calendar</div>
 										</div>
